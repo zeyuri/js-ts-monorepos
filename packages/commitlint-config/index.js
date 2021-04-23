4cccd54dba0d9fc6dab1424f4e49cf0f465fffa6
Zé Yuri <jozeyuri@gmail.com>
2021-04-22 22:54:42 -0300

    feat(commitlint-config): change rule

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -2,10 +2,8 @@ module.exports = {
   rules: {
     "hello-world-rule": (parsed) => {
       const HELLO_WORLD = "Hello World"
-      return [
-        subject.includes(HELLO_WORLD),
-        `Your subject should contain ${HELLO_WORLD} message`,
-      ]
+      console.log(parsed)
+      return [false, `Your subject should contain ${HELLO_WORLD} message`]
     },
   },
 }
