910d7b3c5479e0206bc904043aa150f8f9e54860
Zé Yuri <jozeyuri@gmail.com>
2021-04-22 23:03:18 -0300

    feat(commitlint-plugin-batata): change rule

--- packages/commitlint-plugin-batata/index.js
+++ packages/commitlint-plugin-batata/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   rules: {
-    "hello-world-rule": (parsed) => {
+    "hello-world-rule": (parsed, when, value) => {
       const HELLO_WORLD = "Hello World"
-      console.log(parsed)
+      console.log(parsed, when, value)
       return [false, `Your subject should contain ${HELLO_WORLD} message`]
     },
   },
